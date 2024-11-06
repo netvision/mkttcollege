523ceb9716067c8fc12d0c848a52f7714784b860
rakesh <rakesh@jangid.co.in>
2024-11-06 07:14:47 +0530

    few changes

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { GoogleMap, Marker } from 'vue3-google-map'
 const mapApi = ref(import.meta.env.VITE_MAPKEY)
-const center = { lat: 28.314596, lng: 75.4322150 }
+const center = { lat: 28.377285, lng: 75.411813 }
 </script>
 <template>
     <AppHeader />
@@ -12,13 +12,13 @@ const center = { lat: 28.314596, lng: 75.4322150 }
     :center="center"
     :zoom="16"
     >
-        <Marker :options="{ position: center, title: 'Kothari Mahila Mahavidyalay' }" />
+        <Marker :options="{ position: center, title: 'Malaram Kothari Teachers Training College' }" />
     </GoogleMap>
     <h2 class="text-2xl p-2 px-8 border-b-2">Contact Info:</h2>
-    <p class="text-lg font-bold px-8">Kothari Mahila mahavidyalaya</p>
-    <p class="text-lg px-8">Ward No. 15, Mandrella-333025,</p>
-	<p class="text-lg px-8">Teh. Chirawa, Dist. Jhunjhunu, Rajasthan</p>
-	<p class="text-lg px-8">Phone: +91-9929247066</p>
+    <p class="text-lg font-bold px-8">Malaram Kothari Teachers Training College</p>
+    <p class="text-lg px-8">Rajgarh Road, Sankhantal</p>
+	<p class="text-lg px-8">Teh. Rajgarh, Dist. Churu, Rajasthan</p>
+	<p class="text-lg px-8">Phone: +91-9414540356</p>
     </div>
     <Footer />
 </template>
