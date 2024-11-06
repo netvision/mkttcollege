f115af05611c7ab56fac0bac098e4b567909d1dd
rakesh <rakesh@jangid.co.in>
2024-11-06 06:17:30 +0530

    name changed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -28,7 +28,7 @@ const toggleMobileMenu = () => {
 <template>
 	<header>
 		<div class="bg-gray-400 flex items-end max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-			<h2 class="text-4xl p-3 text-orange-800 pl-8 font-bold">Kothari Mahila Mahavidyalaya, Mandrella</h2>
+			<h2 class="text-4xl p-3 text-orange-800 pl-8 font-bold">Malaram Kothari Teachers Training College, Sankhantal, Churu</h2>
 		</div>
 		<nav class="bg-gray-800">
 			<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup>
 	import slide from '@/assets/building.jpg';
 	useHead({
-		title: 'Kothari Mahila Mahavidyalay',
+		title: 'Malaram Kothari Teachers Training College',
 	});
 	onMounted(async () => {
 		const { Carousel, initTWE } = await import("tw-elements");
