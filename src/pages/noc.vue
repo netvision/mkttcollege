9a0cdc228368d5dac6b33ce4eebebbf2ed0265db
rakesh <rakesh@jangid.co.in>
2024-12-09 14:48:56 +0530

    removed noc page

--- src/pages/noc.vue
+++ src/pages/noc.vue
@@ -7,7 +7,7 @@ import PDF from "pdf-vue3";
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-12">
         <h2 class="text-2xl p-2 px-8">NOC Form Directorate, College Education, Rajasthan</h2>
 		<ul class="text-xl p-2 px-8">
-			<PDF src="./noc.pdf" />
+
 		</ul>
     </div>
     <Footer />
